6a49af49b04bab15a7de3d4dd7589afd346568bf
Nguyen Chuong <chuong.nguyenphucbao@teragroup.io>
2024-01-07 21:35:53 +0700

    Initial Commit

---
+++ lvault-ios/lvault_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lvault_iosApp.swift
+//  lvault-ios
+//
+//  Created by Chuong Nguyen on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct lvault_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
